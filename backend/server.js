a93b86f128c751ccc293494ecc10bb32231094ac
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-02-12 20:16:28 +0200

    create add product endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,35 @@
-import express from "express"
-import dotenv from "dotenv"
-import { connectDB } from "./config/db.js"
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+dotenv.config();
+import Product from "./models/product.model.js";
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get("/products", (req, res) => {
-  res.send("products")
-})
+//middlewares
+app.use(express.json()); // to access json data in req.body
 
-app.listen(port, ()=>{
-  connectDB()
+// Create Product endpoint
+app.post("/api/products", async (req, res) => {
+  const product = await req.body;
+  //console.log(product);
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error Create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  connectDB();
   console.log(`server up & running on http://localhost:${port}`);
-})
+});
