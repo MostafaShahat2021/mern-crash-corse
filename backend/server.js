5b5a510d8558d951d4f85910ec02f2e1b87da3c5
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-02-12 20:38:10 +0200

    Create delete product by id endpoint

--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,43 @@ const port = process.env.PORT || 3000;
 //middlewares
 app.use(express.json()); // to access json data in req.body
 
-// Create Product endpoint
+// Create Product
 app.post("/api/products", async (req, res) => {
   const product = await req.body;
   //console.log(product);
   if (!product.name || !product.price || !product.image) {
     return res
       .status(400)
-      .json({ success: false, message: "Please provide all fields" });
+      .json({ success: false, message: "Please Provide All Fields" });
   }
   const newProduct = new Product(product);
   try {
     await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "Product Created Successfully",
+        data: newProduct,
+      });
   } catch (error) {
     console.error("Error Create product:", error.message);
     res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+// delete product
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  // console.log(id);
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ success: true, message: `Product with id: ${id} is Deleted Successfully` });
+  } catch (error) {
+    console.error("Error deleting product:", error.message);
+    res.status(404).json({ success: false, message: "Product Not Found" });
   }
 });
 
