858ab4f81f50495bf08e5185fddfe2b9cc65656f
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2025-02-13 16:56:36 +0200

    Optimize product route

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,18 +1,19 @@
 import express from "express";
-import { createProduct, deleteProduct, getProducts, updateProduct } from "../controllers/product.controller.js";
+import {
+  createProduct,
+  deleteProduct,
+  getProducts,
+  updateProduct,
+} from "../controllers/product.controller.js";
 
 const router = express.Router();
 
-// Get All Products
-router.get("/", getProducts);
+router.route("/")
+  .get(getProducts)
+  .post(createProduct);
 
-// Create Product
-router.post("/", createProduct);
-
-// Update a Product by id
-router.put("/:id", updateProduct);
-
-// Delete Product By id
-router.delete("/:id", deleteProduct);
+router.route("/:id")
+  .put(updateProduct)
+  .delete(deleteProduct);
 
 export default router;
